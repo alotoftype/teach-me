bb49b37cd442895ffa94483ccaba024427fe6e1e
Serge M <alotoftype@gmail.com>
2019-07-29 18:22:01 -0400

    I want to see errors from my server.

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-const request = require('request');
-const express = require('express');
+const request = require("request");
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
-const RUBY_GEMS_URL = 'https://rubygems.org';
+const RUBY_GEMS_URL = "https://rubygems.org";
 const PORT = 3000;
+
+app.use(morgan("dev", { flags: "a" }));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
 
   next();
 });
 
-app.get('/api/**/*', (req, res) => {
+app.get("/api/**/*", (req, res) => {
   const url = `${RUBY_GEMS_URL}${req.originalUrl}`;
 
   request.get(url, (err, response, body) => {
-    if(response.statusCode === 400) {
+    if (response.statusCode === 400) {
       res.send([]);
     } else {
       res.send(body);
@@ -24,8 +30,8 @@ app.get('/api/**/*', (req, res) => {
   });
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('dist'))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("dist"));
 }
 
 app.listen(PORT);
