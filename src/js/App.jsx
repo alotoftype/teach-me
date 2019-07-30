93528a5cedee5e11dbc3936d9a76d9d9cca1e5f9
Serge M <alotoftype@gmail.com>
2019-07-29 21:51:07 -0400

    Initial App and Test

---
+++ src/js/App.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => <p>This will be my react app</p>;
+
+export default App;
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
+import App from "./App";
 
-const App = () => <p>This will be my react app!</p>;
-
-console.log("application loaded");
 ReactDom.render(<App />, document.getElementById("root"));
-
-export default App;
